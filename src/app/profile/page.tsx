d9edbd5362901d50db3779bfcc1044c36a71b712
wignn <tigfiragnafatur1933@gmail.com>
2025-03-02 09:50:36 +0700

    f

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,31 +1,31 @@
+export const dynamic = "force-dynamic";
+
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import ProfileCard from "@/components/ProfileCard"
+import ProfileCard from "@/components/ProfileCard";
 import { getProfile } from "@/lib/action/user";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
-
 export default async function ProfilePage() {
-  let user, session;
-  try {
-    session = await getServerSession(authOptions)
-    if (session?.id && session?.backendTokens?.accessToken) {
-      user = await getProfile(session.id, session.backendTokens.accessToken)
+  let user = null;
+  let session = await getServerSession(authOptions);
+
+  if (session?.id && session?.backendTokens?.accessToken) {
+    try {
+      user = await getProfile(session.id, session.backendTokens.accessToken);
+    } catch (error) {
+      console.error("❌ Error fetching user profile:", error);
     }
-  } catch (error) {
-    console.error("❌ Error in ProfilePage:", error)
-    
   }
 
   return (
-    <div><Navbar user={user} />
-    <div className="min-h-screen bg-gray-900  flex items-center justify-center p-4 sm:p-6 lg:p-8">
-      
-      <ProfileCard users={user} accessToken={session?.backendTokens.accessToken as string}/>
-     
-    </div> <Footer/>
+    <div>
+      <Navbar user={user} />
+      <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4 sm:p-6 lg:p-8">
+        <ProfileCard users={user} accessToken={session?.backendTokens.accessToken as string} />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
-
