c1db8502b77b22e2ae4fd0ae98d80a26359374f0
wignn <tigfiragnafatur1933@gmail.com>
2025-03-03 19:08:39 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Providers from "./Providers";
 
@@ -21,6 +21,7 @@ export default function RootLayout({
         className={`antialiased`}>
         <Providers>
           {children}
+          <Analytics/>
           <SpeedInsights />
         </Providers>
       </body>
