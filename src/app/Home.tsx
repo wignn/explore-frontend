42c53bc2bbf9377852a5ba56bc5f26798d9cce50
wignn <tigfiragnafatur1933@gmail.com>
2025-04-23 23:33:24 +0700

    dy

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import { bookInterface } from '@/types/book';
 import { UserInterface } from '@/types/user';
 import Footer from '@/components/Footer';
